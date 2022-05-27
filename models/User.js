92f228c8ccbeabc01a131bb316c0b148a9770204
csomervil <carson.somerville@mail.utoronto.ca>
2022-05-26 21:01:17 -0400

    added user model and relationship

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const {Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+class User extends Model {}
+
+// initializing User class
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
+  },
+  {
+    // Using hooks to call functions at asychronous lifecycles
+    hooks: {
+      async beforeCreate(UserData) {
+        newUserData.password = await bcrypt.hash(UserData.password, 10);
+        return UserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,14 +1,35 @@
 const Post = require('./Post');
 const Comment = require('./Comment');
+const User = require('./User');
 
 // Associations
+// Comment belongs to Post User
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
   onDelete: 'SET NULL'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+// Each Post can have many Comments
 Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
+// And the Post must belong to the User
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+// The User can create many Posts have many Comments
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });  
 
-module.exports = { Post, Comment };
+module.exports = { User, Post, Comment };
